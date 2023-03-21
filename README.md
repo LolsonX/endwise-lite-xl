be3809b5a19f5292c457381a93d834710ea22909
LolsonX <xlolsonx@gmail.com>
2023-03-21 11:57:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Endwise plugin for Lite XL editor
+
+Plugin that provides automatic adding of words to end block of codes. (Ex. Ruby if ... end or do ... end syntax)
+
+To use it copy __endwise.lua__ to your plugins folder
+
+## Features
+  * Add end for diffrent languages
+  * Allow to create new rules in Your init.lua file
+  * Enable only for languages You want it to work
+
+## Examples
+
+### Simple config to start given language
+```lua
+local config = require "core.config"
+
+config.plugins.endwise.enable("Ruby")
+```
+### Add custom rules to languages
+```lua
+local config = require "core.config"
+
+config.plugins.endwise.add({
+  name = "Ruby",
+  enabled = true,
+  filename = ".rb$",
+  addition = "end",
+  words = { "module", "class", "def", "if", "unless", "case", "while", "until", "begin", "do" },
+  -- :word: is used to substitute for each word from words list
+  patterns = { "^%s*(:word:).*$", -- :word: starts the line
+               "^%s*%a*%s*(=)%s*(:word:).*$" -- :word: is first word after = sign
+  },
+  -- excludes are regexp that cannot be matched like for example Ruby3.0 endless method definition
+  excludes = { "^%s*(def)%s+%a+%s*(=).*$", -- endless method definition
+               "%s+(end)%s*$" -- line ending with end word
+  }
+})
+
+```
+
+## How it works
+Plugin changes the behaviour of __doc:newline__ command. When You start lite-xl with this plugin enabled You will see that in log:
+`[INFO] Replacing existing command "doc:newline" at $PATH_TO_LITE_XL/core/command.lua:49`
+It attaches new behaviour after original command so it should not break normal behaviour.
+
+## TODO
+ - [x] Append end after correct patterns
+ - [x] Do not append end after excluded pattern
+ - [x] Add possibility to add new languages via init.lua
+ - Languages
+   - [x] Add support for Ruby
+   - [ ] Add support for Lua
+   - [ ] Add support for Elixir
+   - [ ] Add support for Crystal
+   
+## Notes
+  * I am not Lua developer to be fair i know a little bit about it, but i wanted to use Ruby with lite xl end this plugin
+  helps in it
+  * If You find any bug or see possible improvement (especially in terms of patterns) feel free to open a PR or Issue
+  * Thanks to creators of following repositories and pieces of code. I used them as an example and it helped me to create this:
+    * ![lite-xl-lsp](https://github.com/lite-xl/lite-xl-lsp)
+    * ![autoinsert](https://github.com/lite-xl/lite-xl-plugins/blob/master/plugins/autoinsert.lua)
+  * Inspired by ![vim-endwise](https://github.com/tpope/vim-endwise)
---
+++ endwise.lua
@@ -0,0 +1,113 @@
+-- mod-version:3
+local DocView = require "core.docview"
+local Doc = require "core.doc"
+local config = require "core.config"
+local common = require "core.common"
+local core = require "core"
+local command = require "core.command"
+
+local add = function(lang_settings)
+  table.insert(config.plugins.endwise.languages, lang_settings)
+  core.log("Endwise: Added " .. lang_settings.name)
+end
+
+local enable = function(name)
+  local index = nil
+  for idx, lang in ipairs(config.plugins.endwise.languages) do
+    if lang.name == name then index = idx end
+  end
+  
+  if index == nil then
+    core.log("[Error] Endwise: Config for " .. name .. " not found.")
+    return 
+  end
+  
+  config.plugins.endwise.languages[index].enabled = true
+  core.log("Endwise: Enabled endwise for: " .. name)
+end
+
+config.plugins.endwise = common.merge({
+  enabled = true,
+  languages = {
+    {
+      name = "Ruby",
+      enabled = false,
+      filename = ".rb$",
+      addition = "end",
+      words = { "module", "class", "def", "if", "unless", "case", "while", "until", "begin", "do" },
+      -- :word: is used to substitute for each word from words list
+      patterns = { "^%s*(:word:).*$", -- :word: starts the line
+                   "^%s*%a*%s*(=)%s*(:word:).*$" -- :word: is first word after = sign
+      },
+      -- excludes are regexp that cannot be matched like for example Ruby3.0 endless method definition
+      excludes = { "^%s*(def)%s+%a+%s*(=).*$", -- endless method definition
+                   "%s+(end)%s*$" -- line ending with end word
+      }
+    },
+  },
+  enable = enable,
+  add = add,
+  -- The config specification used by the settings gui
+  conf_spec = {
+    description = "Activates auto appending end by default.",
+    path = "enabled",
+    type = "toggle",
+    default = true
+  }
+}, config.plugins.endwise)
+
+local new_line_command = command.map["doc:newline"]
+local match_any_pattern = function(text, patterns, substitutions)
+  for _, pattern in ipairs(patterns) do
+    for _, substitute in ipairs(substitutions) do
+      local to_check = pattern:gsub(":word:", substitute)
+      if(common.match_pattern(text, to_check)) then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+command.add("core.docview", {
+  -- modify current newline command to extend it functionality to be able to
+  -- add addition in new line
+  ["doc:newline"] = function(dv)
+    new_line_command.perform(dv)
+    if config.plugins.endwise.enabled then
+      local matched = false
+      local filename = dv.doc.filename or ""
+      for _idx, settings in ipairs(config.plugins.endwise.languages) do
+        if filename:match(settings.filename) then
+          matched = settings
+          break
+        end
+      end
+
+      -- return early if not match file found or matching file is disabled
+      if not matched then return end
+      if not matched.enabled then return end
+
+      local last_line = dv.doc.lines[dv.last_line1]
+      if match_any_pattern(last_line, matched.patterns, matched.words) and not match_any_pattern(last_line, matched.excludes, matched.words) then
+        local _, indentation_level = last_line:gsub(dv.doc:get_indent_string(), "")
+        local append = (dv.doc:get_indent_string():rep(indentation_level) or "") .. matched.addition
+        DocView.on_text_input(dv, "\n" .. append)
+        dv.doc:move_to(-(#append + 1))
+        DocView.on_text_input(dv, dv.doc:get_indent_string())
+      end
+    end
+  end
+})
+
+
+command.add(nil, {
+  ["endwise:toggle"] = function()
+    config.plugins.endwise.enabled = not config.plugins.endwise.enabled
+    if config.plugins.endwise.enabled then
+      core.log("Endwise: on")
+    else
+      core.log("Endwise: off")
+    end
+  end
+})
